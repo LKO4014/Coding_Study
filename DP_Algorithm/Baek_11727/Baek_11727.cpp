0bca99fe3cdd4ec7e5517875ed6de8cc00d61171
Koeun Lee <dlrhdms9972@gmail.com>
2025-03-07 17:49:42 +0900

    임시저장_21번줄 수정 필요
    vec(3) = vec(2) + vec(1) *  (vec(0)-1)
    vec(4) = vec(3) + vec(2) *  (vec(1)-1)
    이 형식으로 고쳐야함

---
+++ DP_Algorithm/Baek_11727/Baek_11727.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<long> vec;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || i == 1 || i == 2) {
+            if (i == 0)
+                vec.push_back(1);
+            else if (i == 1)
+                vec.push_back(3);
+            else if (i == 2) {
+                vec.push_back(5);
+            }
+        }
+        else {
+            int tmp = vec[i - 1];
+            tmp += vec[i - 2] * (vec[n - i + 2] - 1);
+            vec.push_back(tmp%10007);
+
+        }
+    }
+    cout << vec[vec.size() - 1];
+}
