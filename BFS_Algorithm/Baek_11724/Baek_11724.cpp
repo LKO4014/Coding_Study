dc7b4d3645ab6f3c1ff760528849fe67b1403a09
Koeun Lee <dlrhdms9972@gmail.com>
2025-03-11 17:25:31 +0900

    Baek_11724.cpp Baek_10844.cpp 추가

---
+++ BFS_Algorithm/Baek_11724/Baek_11724.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int returnIndex(vector<int> vec, int tmp) {
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] == tmp)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int tmp1;
+    int tmp2;
+
+    vector<int>Linked;
+    vector<pair<int,int>> input;
+    int count = 0;
+    for (int i = 0; i < m; i++) {
+        cin >> tmp1 >> tmp2;
+        input.push_back(make_pair(tmp1, tmp2));
+    }
+    for (int i = 0; i < m; i++) {
+
+
+        int indexTmp1 = returnIndex(Linked, tmp1);
+        int indexTmp2 = returnIndex(Linked, tmp2);
+
+        if (indexTmp1 == -1 && indexTmp2 != -1) {
+            Linked.push_back(tmp1);
+        }
+        else if (indexTmp1 != -1 && indexTmp2 == -1) {
+            Linked.push_back(tmp2);
+        }
+        else if (indexTmp1 ==-1 && indexTmp2 ==-1){
+            count++;
+            Linked.push_back(tmp1);
+            Linked.push_back(tmp2);
+        }
+        cout << "count : " << count << "\n";
+    }
+    cout << count;
+}
---
+++ DP_Algorithm/Baek_10844/Baek_10844.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int trinum(int n) {
+    return n * (n + 1) / 2;
+}
+
+int main()
+{
+    // 이 문제는 '삼각수'라는 개념을 사용해야 풀 수 있음 (배경 지식)
+
+    vector<int> answer;
+    vector<int> tring;
+    int n;
+    cin >> n;
+    tring.push_back(1);
+    tring.push_back(2);
+    for (int i = 2; i <= n; i++) {
+        tring.push_back(trinum(tring[i - 1]));
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << tring[i] << " ";
+    }
+}
