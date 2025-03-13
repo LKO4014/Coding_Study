4ade8da29a98a5cbb37ed15b5b63675ba86405a2
Koeun Lee <dlrhdms9972@gmail.com>
2025-03-13 17:50:48 +0900

    Baek_

---
+++ General/Baek_2588/Baek_2588.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n, m;
+	cin >> n >> m;
+	int* lst = new int[n];
+
+	int a, b, c;
+
+	for (int i = 0; i < n; i++) {
+		lst[i] = 0;
+	}
+
+	for (int i = 0; i < m;i++) {
+		cin >> a >> b >> c;
+		for (int j = a-1; j < b-1; j++) {
+			lst[j] = c;
+		}
+	}
+
+	for (int i = 0; i<n; i++) {
+		cout << lst[i] << " ";
+	}
+}
---
+++ Sort/Sort_Algorithm/Sort_Algorithm.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void PRINT_VEC(vector<int>& n) {
+    for (int i = 0; i < n.size(); i++) {
+        cout << n[i] << " ";
+    }
+    cout << "\n";
+}
+
+// 버블정렬
+// 어떠한 경우에도 최선, 최악, 평균 모두 시간 복잡도는 O(n^2)
+void bubble(vector<int>& n) {
+    for (int i = 0; i < n.size(); i++) {
+        for (int j = 1; j < n.size() - i;j++) {
+            if (n[j - 1] > n[j]) {
+                swap(n[j - 1], n[j]);
+            }
+        }
+    }
+}
+
+// 선택 정렬
+void select(vector<int>& n) {
+    int indexmin;
+    for (int i = 0; i < n.size() - 1; i++) {
+        indexmin = i;
+        for (int j = i + 1;j < n.size(); j++) {
+            if (n[j] < n[indexmin]) {
+                indexmin = j;
+            }
+        }
+        swap(n[i], n[indexmin]);
+        PRINT_VEC(n);
+    }
+}
+
+// 삽입 정렬
+void insert(vector<int>& n) {
+
+}
+
+int main()
+{
+    vector<int> arr = { 2,3,1,8,9,4 };
+    insert(arr);
+}
