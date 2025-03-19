aa977a3dbecd5a5ac82fdeeb8bb16b2ac5565409
Koeun Lee <dlrhdms9972@gmail.com>
2025-03-19 17:50:00 +0900

    Success_1025 (silver iv)

---
+++ Greedy_Algorithm/Baek_1026/Baek_1026.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int* a = new int[n];
+    int* b = new int[n];
+    int tmp;
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        a[i] = tmp;
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        b[i] = tmp;
+    }
+
+    cout << "\n";
+    sort(a, a+n);
+    sort(b, b + n);
+
+
+    int sum = 0;
+    
+    for (int i = 0; i < n; i++) {
+        tmp = a[i] * b[n - 1 - i];
+        sum += tmp;
+    }
+    cout << sum;
+}
