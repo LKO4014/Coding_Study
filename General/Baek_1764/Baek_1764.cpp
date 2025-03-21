7a071375868d726ba95db9419c500d4c94f90a21
Koeun Lee <dlrhdms9972@gmail.com>
2025-03-21 10:28:49 +0900

    Baek+1764 upload Github_Text_Test

---
+++ General/Baek_1764/Baek_1764.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	vector<string> listen;
+	vector<string> no;
+	string tmp;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		listen.push_back(tmp);
+	}
+	for (int i = 0; i < m; i++) {
+		cin >> tmp;
+		for (int i = 0; i < n; i++) {
+			if (listen[i] == tmp) {
+				no.push_back(tmp);
+			}
+		}
+	}
+
+	cout << no.size();
+	for (int i = 0; i < no.size(); i++) {
+		cout << "\n" << no[i];
+	}
+}
