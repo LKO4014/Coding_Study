ab49f15a1e18dd2ae88ce8dcbe7fca2dd92a8fe2
Koeun Lee <dlrhdms9972@gmail.com>
2025-03-21 17:49:07 +0900

    백준_영화감독 숌 1436_미완

---
+++ General/Baek_1436/Baek_1436.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int init = 6660;
+    int answer;
+    if (n == 1) {
+        cout << "666";
+    }
+    else if (n < 7) {
+        cout << n - 1 << "666";
+    }
+    else {
+        int tmp = n - 7;
+        int count = tmp / 19;
+        tmp %= 19;
+
+        cout << count << " " << tmp << "\n";
+        answer = count * 10000 + init;
+        cout << answer << "\n";
+        if (tmp < 10) {
+            answer += tmp;
+        }
+        else {
+            tmp -= 10;
+            answer += 1006;
+            answer += 1000 * tmp;
+        }
+    }
+    cout << answer;
+}
+
