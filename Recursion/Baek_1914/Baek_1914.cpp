68ffbec3730581a5638d08d5a19bb99593d6c4b7
Koeun Lee <dlrhdms9972@gmail.com>
2025-03-26 17:30:31 +0900

    string으로 count값을 다루어야함. n이 65 이상이 되면 오버플로우가 발생하기 때문.

--- Recursion/Baek_1914/Baek_1914.cpp
+++ Recursion/Baek_1914/Baek_1914.cpp
@@ -1,117 +1,132 @@
 ﻿#include <iostream>
 #include <vector>
 #define VPAIR vector<pair<int,int>>
+#define UINT unsigned long long
+
 using namespace std;
 
-static VPAIR* vec;
-
 // 하노이 횟수 반환
-int COUNT(int a) {
+UINT COUNT(int a) {
     if (a == 1)
         return 1;
     else
         return 2 * COUNT(a - 1) + 1;
 }
 
-// 치환 함수에서 vec[a-1]을 사용함. 인덱스 오류 문제 발생 가능성이 있으니, 이 점 유의하여 풀어야함
-
-// a-1 인덱스의 vector 배열에서 2>>3 , 3>>2로 바꾼 벡터 배열을 반환하는 함수
-VPAIR subsit_first(int a) {
-    vector<pair<int, int>> answer;
-    for (int i = 0; i < vec[a - 1].size(); i++) {
-        int tmp1, tmp2;
-        int tmp_f = vec[a - 1][i].first;
-        int tmp_b = vec[a - 1][i].second;
-
-        if (tmp_f == 2) {
-            tmp1 = 3;
-        }
-        else if (tmp_f == 3) {
-            tmp1 == 2;
-        }
-        else {
-            tmp1 = tmp_f;
-        }
-
-        if (tmp_b == 2) {
-            tmp2 = 3;
-        }
-        else if (tmp_b == 3) {
-            tmp2 = 2;
-        }
-        else {
-            tmp2 = tmp_b;
-        }
-
-        answer.push_back(make_pair(tmp1, tmp2));
+class hn {
+private:
+    int n;
+    VPAIR vec;
+public:
+    hn(int _n) :n(_n) {
+        vec = { {1,3} };
     }
-}
+    // a-1 인덱스의 vector 배열에서 2>>3 , 3>>2로 바꾼 벡터 배열을 반환하는 함수
+    VPAIR subsit_first() {
+        vector<pair<int, int>> answer;
+        for (UINT i = 0; i < vec.size(); i++) {
+            int tmp1, tmp2;
+            int tmp_f = vec[i].first;
+            int tmp_b = vec[i].second;
 
-// a-1 인덱스의 vector 배열에서 1>>2 , 2>>1로 바꾼 벡터 배열을 반환하는 함수
-VPAIR subsit_sec(int a) {
-    vector<pair<int, int>> answer;
-    for (int i = 0; i < vec[a - 1].size(); i++) {
-        int tmp1, tmp2;
-        int tmp_f = vec[a - 1][i].first;
-        int tmp_b = vec[a - 1][i].second;
+            if (tmp_f == 2) {
+                tmp1 = 3;
+            }
+            else if (tmp_f == 3) {
+                tmp1 = 2;
+            }
+            else {
+                tmp1 = tmp_f;
+            }
 
-        if (tmp_f == 1) {
-            tmp1 = 2;
-        }
-        else if (tmp_f == 2) {
-            tmp1 == 1;
-        }
-        else {
-            tmp1 = tmp_f;
-        }
+            if (tmp_b == 2) {
+                tmp2 = 3;
+            }
+            else if (tmp_b == 3) {
+                tmp2 = 2;
+            }
+            else {
+                tmp2 = tmp_b;
+            }
 
-        if (tmp_b == 2) {
-            tmp2 = 1;
+            answer.push_back(make_pair(tmp1, tmp2));
         }
-        else if (tmp_b == 1) {
-            tmp2 = 2;
-        }
-        else {
-            tmp2 = tmp_b;
-        }
-
-        answer.push_back(make_pair(tmp1, tmp2));
-    }
-}
-
-VPAIR add(VPAIR a, VPAIR b) {
-    VPAIR ANSWER;
-    VPAIR mid = { {1,3} };
-    ANSWER.insert(ANSWER.end(), a.begin(), a.end());
-    ANSWER.insert(ANSWER.end(), mid.begin(), mid.end());
-    ANSWER.insert(ANSWER.end(), b.begin(), b.end());
-    return ANSWER;
-}
-
-VPAIR hanoi(int a) {
-    VPAIR answer;
-    if (a == 1) {
-        answer.push_back(make_pair(1, 3));
-        vec[a - 1] = answer;
         return answer;
     }
-    else {
-        //vec[a-1]를 만드는 함수 (재귀)
-        // vec[a-1] = vec[a-2] 의 subsit_first + (1,3) + subsit_sec 
-        // 함수가 반환하지 않고 vec으로 바로 올릴 수 있도록 해야하나? void 형 선언?
-        return 
+    // a-1 인덱스의 vector 배열에서 1>>2 , 2>>1로 바꾼 벡터 배열을 반환하는 함수
+    VPAIR subsit_sec() {
+        vector<pair<int, int>> answer;
+        for (UINT i = 0; i < vec.size(); i++) {
+            int tmp1, tmp2;
+            int tmp_f = vec[i].first;
+            int tmp_b = vec[i].second;
+
+            if (tmp_f == 1) {
+                tmp1 = 2;
+            }
+            else if (tmp_f == 2) {
+                tmp1 = 1;
+            }
+            else {
+                tmp1 = tmp_f;
+            }
+
+            if (tmp_b == 2) {
+                tmp2 = 1;
+            }
+            else if (tmp_b == 1) {
+                tmp2 = 2;
+            }
+            else {
+                tmp2 = tmp_b;
+            }
+
+            answer.push_back(make_pair(tmp1, tmp2));
+        }
+        return answer;
     }
-}
+
+    void add() {
+        VPAIR ANSWER;
+        VPAIR mid = { make_pair(1,3) };
+        VPAIR fir = subsit_first();
+        VPAIR se = subsit_sec();
+
+        ANSWER.insert(ANSWER.end(), fir.begin(),fir.end());
+        ANSWER.insert(ANSWER.end(), mid.begin(), mid.end());
+        ANSWER.insert(ANSWER.end(), se.begin(), se.end());
+        vec = ANSWER;
+    }
+
+    void hanoi() {
+        for (int i = 1; i < n; i++) {
+            add();
+        }
+    }
+
+    void printVec() {
+        for (UINT i = 0; i < vec.size(); i++) {
+            cout << vec[i].first << " " << vec[i].second << "\n";
+        }
+    }
+};
 
 int main()
 {
+
     int n;
     cin >> n;
-    int count = COUNT(n);
+    UINT count = COUNT(n);
 
-    cout << n << " " << count<<"\n";
+    cout << count<<"\n";
 
-    vec = new vector<pair<int,int>>[n];
+    
+    if (n < 21) {
+        hn Hanoi(n);
+        Hanoi.hanoi();
+        Hanoi.printVec();
+    }
+    
 
 }
 
