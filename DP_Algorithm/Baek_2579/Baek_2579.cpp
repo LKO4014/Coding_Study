7c267370bec1861b4c6a37e275584f730a415eb1
Koeun Lee <dlrhdms9972@gmail.com>
2025-03-27 17:50:56 +0900

    미완성_2579

---
+++ DP_Algorithm/Baek_2579/Baek_2579.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int stair;
+    cin >> stair;
+    int score;
+    vector<int> *vec = new vector<int>[stair];
+    for (int i = 0; i < stair; i++) {
+        cin >> score;
+        if (i == 0) {
+            vec[i].push_back(score);
+        }
+        else if (i == 1) {
+            vec[i] = vec[i - 1];
+            vec[i].push_back(score);
+        }
+        else {
+
+        }
+    }
+}
--- Recursion/Baek_1914/Baek_1914.cpp
+++ Recursion/Baek_1914/Baek_1914.cpp
@@ -1,10 +1,70 @@
 ﻿#include <iostream>
 #include <vector>
+#include <algorithm> 
 #define VPAIR vector<pair<int,int>>
 #define UINT unsigned long long
 
 using namespace std;
+string multiply(const string& num, int multiplier);
+void subtractOne(string& num);
+string COUNT(int N) {
+    // 2^N - 1을 구하기 위해 string을 사용
+    if (N == 1) {
+        return "1"; // 2^1 - 1 = 1
+    }
 
+    // 2^N은 2를 N번 곱한 값, 따라서 2^N - 1은 2^N에서 1을 빼면 된다.
+    // 우리는 이 값을 문자열로 반환할 것이므로 pow() 대신 계산을 string으로 처리할 것.
+    string result = "1"; // 시작은 2^1 이므로 1부터 시작
+    for (int i = 1; i < N; ++i) {
+        // 문자열에 2를 곱하는 연산을 구현
+        result = multiply(result, 2);
+    }
+    // 2^N에서 1을 빼기
+    subtractOne(result);
+    return result;
+}
+
+// 문자열로 큰 수를 곱하는 함수 (result = result * 2)
+string multiply(const string& num, int multiplier) {
+    string result = "";
+    int carry = 0;
+
+    // num을 두 배로 늘리기
+    for (int i = num.size() - 1; i >= 0; --i) {
+        int digit = num[i] - '0'; // 문자를 정수로 변환
+        int product = digit * multiplier + carry;
+        result.push_back(product % 10 + '0');
+        carry = product / 10;
+    }
+
+    // carry가 남았다면 결과에 추가
+    while (carry) {
+        result.push_back(carry % 10 + '0');
+        carry /= 10;
+    }
+
+    // 결과를 반대로 뒤집어야 함
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+// 문자열로 큰 수에서 1을 빼는 함수 (num = num - 1)
+void subtractOne(string& num) {
+    int len = num.size();
+    int borrow = 1; // 1을 빼므로 borrow는 1로 설정
+    for (int i = len - 1; i >= 0 && borrow; --i) {
+        if (num[i] == '0') {
+            num[i] = '9';
+        }
+        else {
+            num[i]--;
+            borrow = 0; // 더 이상 빌려올 필요 없음
+        }
+    }
+}
+
+/*
 // 하노이 횟수 반환
 UINT COUNT(int a) {
     if (a == 1)
@@ -12,7 +72,7 @@ UINT COUNT(int a) {
     else
         return 2 * COUNT(a - 1) + 1;
 }
-
+*/
 class hn {
 private:
     int n;
@@ -116,7 +176,7 @@ int main()
 
     int n;
     cin >> n;
-    UINT count = COUNT(n);
+    string count = COUNT(n);
 
     cout << count<<"\n";
 
