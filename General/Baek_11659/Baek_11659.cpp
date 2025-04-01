603c5f905f5ed578fbc03e9730bc75b50e15e257
Koeun Lee <dlrhdms9972@gmail.com>
2025-04-01 17:49:56 +0900

    둘다 못햇음

---
+++ General/Baek_11659/Baek_11659.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	vector<int> num;
+	vector<int> ans;
+	int N;
+	int M;
+	int tmp;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> tmp;
+		num.push_back(tmp);
+	}
+
+	int a, b;
+	int sum = 0;
+
+	for (int i = 0; i < M; i++) {
+		cin >> a >> b;
+		for (int j = a - 1; j < b ; j++) {
+			sum += num[j];
+		}
+		ans.push_back(sum);
+		sum = 0;
+	}
+
+	for (int i = 0; i < M; i++) {
+		cout << ans[i] << "\n";
+	}
+}
--- General/Baek_2869/Baek_2869.cpp
+++ General/Baek_2869/Baek_2869.cpp
@@ -7,5 +7,24 @@ int main()
 
     int m = c - a;
     int l = m / (a - b);
-    cout << l + 1;
+    int lls = m % (a - b);
+    if (lls != 0) {
+        cout << l + 1;
+    }
+    else{
+        cout << l;
+    }
+
+    //another method
+    cout << "\n";
+    int count = 1;
+    int start = a;
+    int tmp = (c - a);
+    cout << "tmp : " << tmp << "\n";
+    if (tmp % 2 != 0) {
+        cout << tmp + 1;
+    }
+    else {
+        cout << tmp + 2;
+    }
 }
