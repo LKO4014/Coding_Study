354681bad2f8fcbeef5df6258d2162f0ecec7599
L KO <koksi9972@naver.com>
2025-04-02 21:50:30 +0900

    solved bronze1

---
+++ General/Baek_1037/Baek_1037/Baek_1037.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long min, max;
+    long long tmp;
+    int n;
+    cin >> n;
+    cin >> tmp;
+    min = tmp;
+    max = tmp;
+    for (int i = 1; i < n; i++) {
+        cin >> tmp;
+        if (min > tmp) {
+            min = tmp;
+        }
+        if (max < tmp) {
+            max = tmp;
+        }
+    }
+
+    tmp = max * min;
+    cout << tmp;
+}
