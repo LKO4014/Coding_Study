d07d2a8d84743ab5342fac2c21d228df70f03c40
L KO <koksi9972@naver.com>
2025-04-03 22:44:57 +0900

    baek_10810 - done

---
+++ General/Baek_10810/Baek_10810/Baek_10810.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int* bucket = new int[n];
+    for (int i = 0; i < n; i++) {
+        bucket[i] = 0;
+    }
+
+    int a, b, c;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b >> c;
+        for (int j = a-1; j < b; j++) {
+            
+            bucket[j] = c;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << bucket[i] << " ";
+    }
+}
