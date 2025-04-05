48467eda540b8bcd72c7ce943e5a8f51222318c5
L KO <koksi9972@naver.com>
2025-04-06 00:53:59 +0900

    2775, 10813 / bronze 1, 2 (완료)

---
+++ DP_Algorithm/Baek_2775/Baek_2775/Baek_2775.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int people(int top, int room) {
+	if (room == 1) {
+		return 1;
+	}
+	if (top == 0) {
+		return room;
+	}
+	else {
+		return people(top - 1, room) + people(top, room-1);
+	}
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		int top, room;
+		cin >> top >> room;
+		cout << people(top, room) << "\n";
+	}
+}
---
+++ General/Baek_10813/Baek_10813/Baek_10813.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> num;
+    for (int i = 0; i < n; i++) {
+        num.push_back(i + 1);
+    }
+    int tmp;
+    int a, b;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        if (a != b) {
+            tmp = num[a - 1];
+            num[a - 1] = num[b - 1];
+            num[b - 1] = tmp;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << num[i] << " ";
+    }
+}
