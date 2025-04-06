d302fe8c23cf752aef0fa7ecefca178dee9399df
L KO <koksi9972@naver.com>
2025-04-07 02:11:20 +0900

    둘다 못품ㅠ

---
+++ Brute_Force_Algorithm/Baek_1439/Baek_1439/Baek_1439.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 666;
+	int n;
+	cin >> n;
+	if (n <= 6) {
+		num += (n - 1) * 1000;
+	}
+	else {
+		num = 6660;
+		n -= 6;
+		int a = n / 20;
+		int b = n % 20;
+		
+		num += a * 10000;
+		if (b < 10) {
+			num += b;
+		}
+		else {
+			num += 1000 * b;
+		}
+	}
+	cout << num;
+}
+
---
+++ General/Baek_4949/Baek_4949/Baek_4949.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<pair<int, string>> stu;
+
+int main()
+{
+	int n;
+	int age;
+	string name;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> age >> name;
+		stu.push_back(make_pair(age, name));
+	}
+
+	sort(stu.begin(), stu.end());
+	for (int i = 0; i < n; i++) {
+		cout << stu[i].first << " " << stu[i].second << "\n";
+	}
+}
