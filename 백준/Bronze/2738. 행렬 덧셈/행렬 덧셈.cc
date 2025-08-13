e48dd63c2a7e9f2bc47a2bec548fcf9d2045b41b
L KO <koksi9972@naver.com>
2025-08-13 10:31:06 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+	int** a = new int*[n];
+	int** b = new  int*[n];
+	for (int i = 0; i < n; i++) {
+		a[i] = new int[m];
+		b[i] = new int[m];
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> a[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> b[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout<< a[i][j] + b[i][j]<<" ";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
